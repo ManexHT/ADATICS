2073e914059dd687584866b85a9a709caf112810
Manu <jlopez221104@gmail.com>
2025-03-09 18:17:15 -0600

    Se agrego PlaneacionController.java al api, requiere de la existencia de la clase Reporte para continuar

---
+++ src/main/java/ryuulogic/planeacion/PlaneacionController.java
@@ -0,0 +1,65 @@
+package ryuulogic.planeacion;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import javax.swing.text.html.Option;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/planeacion")
+public class PlaneacionController {
+    @Autowired
+    private PlaneacionRepository planeacionRepository;
+
+    /*Hay que sincronizar con ReporteRepository
+    @Autowired
+    private ReporteRepository reporteRepository
+     */
+
+    @GetMapping()
+    public ResponseEntity<Iterable<Planeacion>> findAll() {
+        return ResponseEntity.ok(planeacionRepository.findAll());
+    }
+
+    @GetMapping("/{idPlaneacion}")
+    public ResponseEntity<Planeacion> findById(@PathVariable Long idPlaneacion) {
+        Optional<Planeacion> planeacionOptional = planeacionRepository.findById(idPlaneacion);
+        if (planeacionOptional.isPresent()) {
+            return ResponseEntity.ok(planeacionOptional.get());
+        }else{
+            return ResponseEntity.notFound().build();
+        }
+    }
+
+    /* REQUIERE DE LA EXISTENCIA DE Reportes
+    @PostMapping
+    public ResponseEntity<Planeacion> create(@RequestBody Planeacion planeacion, UriComponentsBuilder uriBuilder) {
+        Optional<>
+    }
+
+    @PutMapping
+    */
+
+    @DeleteMapping("/{idPlaneacion}")
+    public ResponseEntity<Void> delete(@PathVariable Long idPlaneacion) {
+        if(planeacionRepository.findById(idPlaneacion).isPresent()) {
+            planeacionRepository.deleteById(idPlaneacion);
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.notFound().build();
+    }
+
+    /* REQUIERE DE LA EXISTENCIA DE REPORTES
+    @GetMapping("/reportes/{idPlaneacion}")
+    public ResponseEntity<Iterable<Reporte>tResporte>(@PathVariable Long idPlaneacion){
+        Optional<Planeacion> planeacionOptional = planeacionRepository.findById(idPlaneacion);
+        if (planeacionOptional.isPresent()) {
+            return ResponseEntity.ok(planeacionOptional.get().getReportes());
+        }
+        return ResponseEntity.notFound().build();
+    }
+     */
+}
